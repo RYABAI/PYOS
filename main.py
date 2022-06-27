0d529de06f476bc60e4b88728f22235c4ef8906b
RYABAI <63951716+RYABAI@users.noreply.github.com>
2022-06-27 21:11:42 +0400

    BUG FIX
    bug fix

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 print("WELCOME TO OPERATING SYSTEM")
 print("V 0.1")
-print("")
-def commandLoop(additionaltext):
-    print(additionaltext)
-    global cmd == input("Enter Command: ")
+cmd = None
+def cmdl(at):
+    print(at)
+    cmd = input("Enter Command: ")
 
-commandLoop()
 if cmd == "Save test":
-    with open ("Save.txt", "a") as file:
+    with open("Save.txt", "a") as file:
         file.write("Save Test\n")
         file.close()
-        pass
-    commandLoop("Modified Save file")
+    cmdl("Saved")
+elif cmd == None:
+    print("")
+    cmdl("")
 else:
-    commandLoop("No command found")
+    cmdl("Failed")
